0698db1fd1bd9667aebb4e946bc296185a643012
Muhammad shafey kamran <slashmonkey0@gmail.com>
2026-05-02 19:41:53 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,24 @@
 import urllib.request
 import json
+class RepoTracker:
+	def __init__(repoName):
+		self.repoName=repoName
+		self.count=0
+	def countIncrease():
+		self.count+=1
 username=input("Enter username: ")
 request = urllib.request.urlopen(f"https://api.github.com/users/{username}/events")
 data = request.read()        
 data = json.loads(data)      
-pushevents=0
 eventrepo=set()
+repoTracker=[]
 for event in data:
     if event["type"]=="PushEvent":
-        eventrepo.add(event["repo"]["name"])
+        if event["repo"]["name"] in eventrepo:
+        	
+        
+        else:
+        eventrepo.append(RepoTracker(event["repo"]["name"]))
         
 
    
